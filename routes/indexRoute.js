4c3601bfc3328d9cad5209420bc17272abfacb10
Octavio Villena <octaviovillena@hotmail.com>
2021-04-28 23:27:21 -0700

    debug github strat

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
+const crypto = require("crypto");
+
+const Joi = require("joi");
+const { ObjectId } = require("bson");
 
 router.get("/", ensureAuthenticated, (req, res) => {
   res.render("index");
@@ -9,6 +13,5 @@ router.get("/", ensureAuthenticated, (req, res) => {
 router.get("/createChallenge", (req, res) => {
   res.render("createChallenge");
 });
-
 
 module.exports = router;
