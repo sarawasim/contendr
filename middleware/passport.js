4d0250df82241b33867f682804f8f93b506dafb7
Octavio Villena <octaviovillena@hotmail.com>
2021-04-29 00:01:14 -0700

    github works i think.

--- middleware/passport.js
+++ middleware/passport.js
@@ -46,8 +46,8 @@ passport.serializeUser(async function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  let user = userController.getUserById(id);
+passport.deserializeUser(async function (id, done) {
+  let user = await userController.getUserById(id);
   if (user) {
     done(null, user);
   } else {
