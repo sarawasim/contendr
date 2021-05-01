5df10e20d2aafbf65e50faab7343462348e514e9
Octavio Villena <octaviovillena@hotmail.com>
2021-04-30 20:50:03 -0700

    added aut middleware to other index routes

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
 const { upload } = require("../middleware/upload");
-const {path} = require('path')
+const { path } = require("path");
 
 const crypto = require("crypto");
 
@@ -13,15 +13,20 @@ router.get("/", ensureAuthenticated, (req, res) => {
   res.render("index");
 });
 
-router.get("/createChallenge", (req, res) => {
+router.get("/createChallenge", ensureAuthenticated, (req, res) => {
   res.render("createChallenge");
 });
 
-router.post("/createChallenge", upload.single('fileUpload'), (req, res) => {
-  console.log("testing req.body")
-  console.log(req.file.filename)
-  let filename = req.file.filename
-  res.render("test", {image: "tempImages/"+filename})
-})
+router.post(
+  "/createChallenge",
+  ensureAuthenticated,
+  upload.single("fileUpload"),
+  (req, res) => {
+    console.log("testing req.body");
+    console.log(req.file.filename);
+    let filename = req.file.filename;
+    res.render("test", { image: "tempImages/" + filename });
+  }
+);
 
 module.exports = router;
