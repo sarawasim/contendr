c53622cec0e88271dc35d87570258e06c3bed37f
Octavio Villena <octaviovillena@hotmail.com>
2021-05-09 23:16:15 -0700

    likes can visibly change on the page on click

---
+++ public/scripts/post.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  $(".likes-container")
+    .find("a")
+    .on("click", function (e) {
+      e.preventDefault();
+
+      if ($(this).find("i").hasClass("fas")) {
+        $(this).find("i").removeClass("fas");
+        $(this).find("i").addClass("far");
+        let likesNum = $(this).parent().find("p.likes").text();
+        $(this)
+          .parent()
+          .find("p.likes")
+          .text(`${parseInt(likesNum) - 1}`);
+        console.log($(this).parent().find("p.likes").text());
+      } else {
+        $(this).find("i").removeClass("far");
+        $(this).find("i").addClass("fas");
+        let likesNum = $(this).parent().find("p.likes").text();
+        $(this)
+          .parent()
+          .find("p.likes")
+          .text(`${parseInt(likesNum) + 1}`);
+        console.log($(this).parent().find("p.likes").text());
+      }
+      console.log("the likes button was pressed");
+    });
+});
