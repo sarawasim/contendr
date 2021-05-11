0762688739201688548db27edf875b26f0fa1592
Octavio Villena <octaviovillena@hotmail.com>
2021-05-10 23:31:16 -0700

    login page wave rework

--- public/scripts/challengeForm.js
+++ public/scripts/challengeForm.js
@@ -8,43 +8,42 @@
 // }
 // import {getUsername} from '../../controllers/userControllerMongo'
 
-
-$(document).ready(function(){
-  $('.toggleSearch').click(function() {
-    let search = $(this).prev().val()
+$(document).ready(function () {
+  $(".toggleSearch").click(function () {
+    let search = $(this).prev().val();
     // console.log(search)
     // let results = test.getUsername(search)
     // console.log(results)
-    $('#dropSearch').toggle()
-  })
+    $("#dropSearch").toggle();
+  });
 
   // function searchUsername(name) {
   //   const result = await $.get(`/createChallenge/username?search=${name}`)
   //   console.log(result)
   // }
 
-  const $searchInput = $("#searchUser")
-  $searchInput.on("input", ()=> {
-    const input = $searchInput.val()
-    const list = $("#followingList").val()
-    let newList = list.split(',')
+  const $searchInput = $("#searchUser");
+  $searchInput.on("input", () => {
+    const input = $searchInput.val();
+    const list = $("#followingList").val();
+    let newList = list.split(",");
 
     const results = $.grep(newList, (name) => {
-      return name.includes(input)
-    })
+      return name.includes(input);
+    });
 
     const li = results.map((name) => {
-      return $(`<li class="results" value="${name}">${name}</li>`)
-    })
+      return $(`<li class="results">${name}</li>`);
+    });
 
-    $("#searchResults").empty()
-    $("#searchResults").append(li)
-    console.log(li)
-  })
+    $("#searchResults").empty();
+    $("#searchResults").append(li);
+    console.log(li);
+  });
 
   $("body").on("click", ".results", function () {
-    console.log($(this).text())
-    $($searchInput).val($(this).text()) 
-    $("#searchResults").empty()
-  })
-})
+    console.log($(this).text());
+    $($searchInput).val($(this).text());
+    $("#searchResults").empty();
+  });
+});
