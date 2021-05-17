e31d43e6cf5497c677bb5a5a58774adc05a6c338
Octavio Villena <octaviovillena@hotmail.com>
2021-05-17 00:25:24 -0700

    form validation implemented. alerted with missing input

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -4,108 +4,80 @@ const submitForm = document.getElementById("submitForm");
 
 submitForm.addEventListener("click", async (event) => {
   event.preventDefault();
-  console.log(JSON.stringify(challengeInput));
-  const file = challengeInput.files[0];
 
-  //get secure url from our Server
-  const { url } = await fetch("/s3Url").then((res) => res.json());
-  console.log(url);
+  const inputMessage = missingInputs();
 
-  //post image directly to the s3 bucket
-  await fetch(url, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    body: file,
-  });
+  if (inputMessage) {
+    alert(inputMessage);
+  } else {
+    const file = challengeInput.files[0];
 
-  //get s3 imageURL
-  const imageURL = url.split("?")[0];
-  console.log(imageURL);
+    //get secure url from our Server
+    const { url } = await fetch("/s3Url").then((res) => res.json());
+    console.log(url);
 
-  // append this imageURL to hidden input somewhere on createChallenge.ejs
-  let URLinput = document.createElement("INPUT");
-  URLinput.setAttribute("type", "hidden");
-  URLinput.setAttribute("value", imageURL);
-  URLinput.setAttribute("name", "imageURL");
-  let hiddenURL = document.getElementById("hiddenURL");
-  hiddenURL.appendChild(URLinput);
-  console.log(URLinput);
+    //post image directly to the s3 bucket
+    await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body: file,
+    });
 
-  // submit form
-  challengeForm.submit();
-  // const searchUser = $("input#searchUser");
+    //get s3 imageURL
+    const imageURL = url.split("?")[0];
+    console.log(imageURL);
 
-  // const cat1 = $("input#cat1");
-  // const cat2 = $("input#cat2");
-  // const cat3 = $("input#cat3");
-  // const cat4 = $("input#cat4");
-  // const cat5 = $("input#cat5");
-  // const cat6 = $("input#cat6");
+    // append this imageURL to hidden input somewhere on createChallenge.ejs
+    let URLinput = document.createElement("INPUT");
+    URLinput.setAttribute("type", "hidden");
+    URLinput.setAttribute("value", imageURL);
+    URLinput.setAttribute("name", "imageURL");
+    let hiddenURL = document.getElementById("hiddenURL");
+    hiddenURL.appendChild(URLinput);
+    console.log(URLinput);
 
-  // let catRadio = false;
-  // if (
-  //   cat1.is(":checked") ||
-  //   cat2.is(":checked") ||
-  //   cat3.is(":checked") ||
-  //   cat4.is(":checked") ||
-  //   cat5.is(":checked") ||
-  //   cat6.is(":checked")
-  // ) {
-  //   catRadio = true;
-  // }
-
-  // let timeRadio = false;
-  // const time43200s = $("input#time43200s");
-  // const time86400s = $("input#time86400s");
-  // if (time43200s.is(":checked") || time86400s.is(":checked")) {
-  //   timeRadio = true;
-  // }
-
-  // const titleInput = $("input#titleInput");
-  // const descriptionInput = $("input#descriptionInput");
-
-  // challengeForm.submit(function (event) {
-  //   if (
-  //     searchUser.value == "" ||
-  //     !cat_radio ||
-  //     !timeRadio ||
-  //     titleInput.value == "" ||
-  //     descriptionInput.value == ""
-  //   ) {
-  //     return false;
-  //   }
-  // });
-
-  // Initialize form validation on the registration form.
-  // It has the name attribute "registration"
-  // $("#challengeForm").validate({
-  //   // Specify validation rules
-  //   rules: {
-  //     // The key name on the left side is the name attribute
-  //     // of an input field. Validation rules are defined
-  //     // on the right side
-  //     searchUser: "required",
-  //     categoryInput: "required",
-  //     timeInput: "required",
-  //     titleInput: "required",
-  //     descriptionInput: "required",
-  //     fileUpload: "required",
-  //   },
-  //   // Specify validation error messages
-  //   messages: {
-  //     searchUser: "Please enter a user to challenge",
-  //     categoryInput: "Please select a category",
-  //     timeInput: "Please select a time limit",
-  //     titleInput: "Please enter a title",
-  //     descriptionInput: "Please enter a description",
-  //     fileUpload: "please upload an image or gif",
-  //   },
-  //   // Make sure the form is submitted to the destination defined
-  //   // in the "action" attribute of the form when valid
-  //   submitHandler: function (form) {
-  //     form.submit();
-  //   },
-  // });
+    // submit form
+    challengeForm.submit();
+  }
 });
+
+function missingInputs() {
+  const searchUser = document.forms["challengeForm"]["searchUser"].value;
+
+  const category = document.getElementsByName("categoryInput");
+  let catValue;
+  for (let i = 0; i < category.length; i++) {
+    if (category[i].checked) {
+      catValue = category[i].value;
+    }
+  }
+
+  const time = document.getElementsByName("timeInput");
+  let timeValue;
+  for (let i = 0; i < time.length; i++) {
+    if (time[i].checked) {
+      timeValue = time[i].value;
+    }
+  }
+
+  const title = document.forms["challengeForm"]["titleInput"].value;
+  const description = document.forms["challengeForm"]["descriptionInput"].value;
+  const fileUpload = document.forms["challengeForm"]["fileUpload"].value;
+
+  if (
+    (searchUser == "" ||
+      !catValue ||
+      !timeValue ||
+      title == "" ||
+      description == "") &&
+    fileUpload == ""
+  ) {
+    return "Please fill out entire form and upload a photo/GIF";
+  } else if (fileUpload == "") {
+    return "Please upload a photo/GIF";
+  } else {
+    return "Please fill out entire form";
+  }
+}
