adf36ee428f8bef2e83951c1969294997af247d3
Octavio Villena <octaviovillena@hotmail.com>
2021-05-20 17:18:20 -0700

    file type accept

--- public/scripts/accept.js
+++ public/scripts/accept.js
@@ -1,6 +1,6 @@
 const updateP2URL = document.querySelector("#updateP2URL");
 const p2Input = document.querySelector(".p2Input");
-const submitForm = document.getElementById("submitForm")
+const submitForm = document.getElementById("submitForm");
 
 submitForm.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -9,24 +9,23 @@ submitForm.addEventListener("click", async (event) => {
   //get secure url from our Server
   const { url } = await fetch("/s3Url").then((res) => res.json());
 
-  
   //post image directly to the s3 bucket
   await fetch(url, {
     method: "PUT",
     headers: {
-      "Content-Type": "multipart/form-data",
+      "Content-Type": file.type,
     },
     body: file,
   });
 
   //get s3 imageURL
   const imageURL = url.split("?")[0];
-  
+
   // append this imageURL to hidden input somewhere on createChallenge.ejs
   let URLinput = document.createElement("INPUT");
   URLinput.setAttribute("type", "hidden");
   URLinput.setAttribute("value", imageURL);
-  URLinput.setAttribute("name", "imageURL")
+  URLinput.setAttribute("name", "imageURL");
   let hiddenURL = document.getElementById("hiddenURL");
   hiddenURL.appendChild(URLinput);
 
@@ -35,7 +34,7 @@ submitForm.addEventListener("click", async (event) => {
   fileInput.setAttribute("value", file.type);
   fileInput.setAttribute("name", "fileTypeInput");
   let hiddenInput = document.getElementById("fileType");
-  hiddenInput.appendChild(fileInput)
-  // submit form 
+  hiddenInput.appendChild(fileInput);
+  // submit form
   updateP2URL.submit();
 });
